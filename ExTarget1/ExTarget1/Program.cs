196ac66983b8cdc51d26c8d274ec3c8013c23b3a
Enrico Candido <enrico.enr07@gmail.com>
2023-02-27 16:21:04 -0300

    add ex

---
+++ ExTarget1/ExTarget1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ExTarget1 {
+    class Program {
+        static void Main(string[] args) {
+            int indice, soma, k;
+            indice = 13;
+            soma = 0;
+            k = 0;
+
+            while (k < indice) {
+                k += 1;
+                soma += k;
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
