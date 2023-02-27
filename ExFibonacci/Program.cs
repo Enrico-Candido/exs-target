12965233225c8780c671ba145c75c38b58f237ba
Enrico Candido <enrico.enr07@gmail.com>
2023-02-27 16:24:15 -0300

    add ex

---
+++ ExFibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExFibonacci {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite um numero inteiro: ");
+            int number = int.Parse(Console.ReadLine());
+
+            List<int> list = new List<int>();
+            list.Add(0);
+            list.Add(1);
+
+            int aux = 0;
+            while (aux < number) {
+                list.Add(list[aux] + list[aux+1]);
+                aux++;
+            }
+            
+            int pertence = list.Find(x => x == number);
+            if (pertence == number) Console.WriteLine("Pertence a sequencia de Fibonacci");
+            else Console.WriteLine("Nao pertence a sequencia de Fibonacci");
+        }
+    }
+}
