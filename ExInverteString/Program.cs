2ec3a7e2372c986ef165b841b860be2bc9e53ea5
Enrico Candido <enrico.enr07@gmail.com>
2023-02-27 21:21:58 -0300

    add ex

---
+++ ExInverteString/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExInverteString {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite uma palavra/frase: ");
+            string str = Console.ReadLine();
+
+            for (int i = str.Length - 1; i < str.Length && i >= 0; i--) {
+                Console.Write(str[i]);
+            }
+        }
+    }
+}
