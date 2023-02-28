1081f8efa414cd0acc7bcac53b57ff2dc629bdd6
Enrico Candido <enrico.enr07@gmail.com>
2023-02-27 21:19:49 -0300

    add ex

---
+++ ExFaturamentoMensal/ExFaturamentoMensal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExFaturamentoMensal {
+    class Program {
+        static void Main(string[] args) {
+            List<Unidade> list = new List<Unidade>();
+            list.Add(new Unidade("SP", 67836.43));
+            list.Add(new Unidade("RJ", 36678.66));
+            list.Add(new Unidade("MG", 29229.88));
+            list.Add(new Unidade("ES", 27165.48));
+            list.Add(new Unidade("Outros", 19849.53));
+
+            double somaTotal = 0;
+            foreach (Unidade unidade in list) {
+                somaTotal += unidade.ValorArrecadado;
+            }
+
+            foreach (Unidade unidade in list) {
+                double percentual = unidade.ValorArrecadado * 100 / somaTotal;
+                Console.WriteLine($"Local: {unidade.Local}, valor arrecadado em percentual por unidade: {percentual:F2}%");
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ ExFaturamentoMensal/ExFaturamentoMensal/Unidade.cs
@@ -0,0 +1,11 @@
+﻿namespace ExFaturamentoMensal {
+    class Unidade {
+        public string Local { get; set; }
+        public double ValorArrecadado { get; set; }
+        public Unidade() { }
+        public Unidade(string local, double valorArrecadado) {
+            Local = local;
+            ValorArrecadado = valorArrecadado;
+        }
+    }
+}
